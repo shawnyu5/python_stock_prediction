27a5246b14b1a32c9dac922a4b38493afc87cfdf
Shawn Yu <shawn.yu5@hotmail.com>
2022-02-28 22:49:06 -0500

    feat: data converted into pandas data frame

--- prediction.py
+++ prediction.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from pprint import pprint
 from average_true_range_values import Average_true_range_values
 from daily_timeSeries import Daily_timeSeries
+import pandas as pd
 
 
 def sync_dates(dict1, dict2):
@@ -26,7 +27,7 @@ def sync_dates(dict1, dict2):
         # if not same:
         # to_remove.append(date1)
 
-    pprint(to_remove)
+    # pprint(to_remove)
     # remove dates from both dictionary
     for date in to_remove:
         del dict1[date]
@@ -47,11 +48,41 @@ symbol = "lspd"
 dt = Daily_timeSeries(symbol=symbol)
 # pprint(dt.get_all())
 
-av = Average_true_range_values(symbol=symbol)
-# pprint(av.get_all())
+av = Average_true_range_values(symbol=symbol).get_all()
 
-dict1, dict2 = sync_dates(dt.get_all(), av.get_all())
+
+# dict1, dict2 = sync_dates(dt.get_all(), av.get_all())
 # pprint(dt.get_all().keys())
+
+x = []
+Y = []
+for key, value in dt.get_all().items():
+    # {'1. open': '73.8300', '2. high': '74.6500', '3. low': '71.1500', '4. close': '71.2700', '5. volume': '371926'}
+    obj = {}
+    try:
+        obj["date"] = key
+        obj["open"] = value["1. open"]
+        obj["high"] = value["2. high"]
+        obj["low"] = value["3. low"]
+        if key in av:
+            price = list(av[key].values())[0]
+            obj["atr"] = price
+        x.append(obj)
+
+        Y.append((obj["date"], value["4. close"]))
+        # Y.append(value["4. close"])
+
+    except:
+        continue
+
+# pprint(x)
+# pprint(Y)
+
+feature_df = pd.DataFrame(data=x, columns=["date", "high", "low", "open", "atr"])
+label_df = pd.DataFrame(data=Y, columns=["date", "closing"])
+print(feature_df.head())
+print(label_df.head())
+
 
 # if dict1 not in list(dt.get_all()):
 # print("Ayyyy")
