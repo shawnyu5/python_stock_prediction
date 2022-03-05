0b6c51d1dc3a6faf708cf4f7f8e87fb5ae07431e
Shawn Yu <shawn.yu5@hotmail.com>
2022-03-05 12:52:22 -0500

    model evaluated

--- prediction.py
+++ prediction.py
@@ -26,72 +26,51 @@ dt = Daily_timeSeries(symbol=symbol)
 av = Average_true_range_values(symbol=symbol).get_all()
 
 
-x = []
-Y = []
+data = []
 for key, value in dt.get_all().items():
     # {'1. open': '73.8300', '2. high': '74.6500', '3. low': '71.1500', '4. close': '71.2700', '5. volume': '371926'}
     obj = {}
     try:
         obj["date"] = key
         obj["open"] = float(value["1. open"])
-        obj["high"] = value["2. high"]
+        obj["high"] = float(value["2. high"])
         obj["low"] = float(value["3. low"])
+        obj["close"] = float(value["4. close"])
         if key in av:
             price = list(av[key].values())[0]
             obj["atr"] = float(price)
-            # print(type(price))
-        x.append(obj)
-
-        Y.append(value["4. close"])
-        # Y.append(value["4. close"])
+        data.append(obj)
 
     except:
         continue
 
-# pprint(x)
-# pprint(Y)
+train, test = train_test_split(data, test_size=0.20, shuffle=True)
+x_train_df = pd.DataFrame(data=train, columns=["high", "low", "open", "atr"])
+y_train_df = pd.DataFrame(data=train, columns=["close"])
 
-# train, test = train_test_split(x, test_size=0.20, shuffle=True)
-x_train = pd.DataFrame(data=x, columns=["date", "high", "low", "open", "atr"])
-y_train = pd.DataFrame(data=Y, columns=["closing"])
-x_train.drop("date", axis=1, inplace=True)
-# print(x_train.info())
-# exit()
-# print(x_train.head())
+x_test_df = pd.DataFrame(data=test, columns=["high", "low", "open", "atr"])
+y_test_df = pd.DataFrame(data=test, columns=["close"])
 
-x_train = x_train.to_numpy().astype("float32")
+#  print(x_train_df.head())
+#  print(y_train_df.head())
+
+x_train_df = x_train_df.to_numpy().astype("float32")
 # convert all y_train values to floats
-y_train = [float(current) for current in y_train.closing.values]
-y_train = np.array(y_train)
-
-
-# print(x_train[0])
-
-# feature_df = np.column_stack(
-# (
-# feature_df.date.values,
-# feature_df.high.values,
-# feature_df.low.values,
-# feature_df.open.values,
-# feature_df.atr.values,
-# )
-# )
-# print(x_train.shape)
-# print(y_train.shape)
-# print(f" x_train.shape[0]: {str(x_train.shape[0])}")  # __AUTO_GENERATED_PRINT_VAR__
-# x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
-# y_train = y_train.reshape((y_train.shape[0], y_train.shape[1], 1))
-
-# print(x_train)
-# print(y_train.closing.values)
-
-# exit()
+#  y_train_df = [float(current) for current in y_train_df.closing.values]
+y_train_df = np.array(y_train_df)
 
 
 # Building the LSTM Model
 model = keras.Sequential(
     [
-        (keras.layers.LSTM(units=32, return_sequences=True, input_shape=(x_train.shape[1], 1), activation="relu")),
+        (
+            keras.layers.LSTM(
+                units=32,
+                return_sequences=True,
+                input_shape=(x_train_df.shape[1], 1),
+                activation="relu",
+            )
+        ),
         (keras.layers.LSTM(units=32, return_sequences=True, activation="relu")),
         (keras.layers.Dense(units=1, activation="relu")),
         # (keras.layers.Dense(units=16, activation="sigmoid")),
@@ -101,11 +80,10 @@ model = keras.Sequential(
 
 model.compile(optimizer="adam", loss="mean_squared_error")
 
-print(f" x_train: {str(x_train)}")  # __AUTO_GENERATED_PRINT_VAR__
-print(f" y_train: {str(y_train)}")  # __AUTO_GENERATED_PRINT_VAR__
 
-model.fit(x_train, y_train, epochs=50, batch_size=4, verbose=1)
+model.fit(x_train_df, y_train_df, epochs=50, batch_size=4, verbose=1)
 
 # testing
-
+print("EVALUATION")
+model.evaluate(x_test_df, y_test_df)
 
